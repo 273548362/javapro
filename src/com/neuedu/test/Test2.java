0416677ff5ff32735f3dc1e58879d063729668ff
Chen <273548362@qq.com>
2020-02-07 15:32:15 +0800

    ab

---
+++ src/com/neuedu/test/Test2.java
@@ -0,0 +1,7 @@
+package com.neuedu.test;
+
+/**
+ * Created by Chen on 2020/2/7.
+ */
+public class Test2 {
+}
--- src/com/neuedu/test/Test3.java
+++ src/com/neuedu/test/Test3.java
@@ -3,8 +3,8 @@ package com.neuedu.test;
 /**
  * Created by Chen on 2020/2/7.
  */
-public class Test {
+public class Test3 {
     public static void main(String[] args) {
-
+        int a =10;
     }
 }
